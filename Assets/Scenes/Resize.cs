bb83b9bc210f203014c649f8b830a1000eac82f8
Alison Dewancker <alison.dewancker@gmail.com>
2019-05-26 19:58:14 -0400

    Single scene folder and Double scene folder, created small and large size examples, added rigid object to camera in Single_1l and need to do so for any scene with large bag. Changed cloth properties for small bag, to be tested colliding with others

---
+++ Assets/Scenes/Resize.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resize : MonoBehaviour
+{
+    // Use this for initialization
+  
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += new Vector3(0.02F, 0.01F, 0);
+    }
+}
